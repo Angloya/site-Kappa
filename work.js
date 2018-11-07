24465e7710edca2515159ba80e4bb2769a9f3566
Alena Borisova <alena.borisova@altenar.com>
2018-11-07 12:50:48 +0300

    fixed bugs

--- work.js
+++ work.js
@@ -2,17 +2,34 @@ const Project = new Vue({
     el: '#project',
     data: {
         title: 'Project Info',
-       name: 'premiumlayers',
-      likes: '138 Likes' ,
-    date:'25 December, 2013',
+        name: 'premiumlayers',
+        likes: '138 Likes',
+        date: '25 December, 2013',
         comments: '4 comments',
-        filters: ['web design', 'photograpgy', 'development', 'php'],
+        tags: 'Tags',
+        types: [{
+            tag: 'web design'
+        }, {
+            tag: 'photograpgy'
+        }, {
+            tag: 'development'
+        }, {
+            tag: 'php'
+        }],
         title3: 'Project Gallery',
         title4: 'Progect Features',
-        items:[{info:'Responsive Layout' },
-      { info: 'Font Awesome Icons' },
-     {info: 'Clean & Commented Code' },
-            {info: 'Highly Customizable' }
+        items: [{
+                info: 'Responsive Layout'
+            },
+            {
+                info: 'Font Awesome Icons'
+            },
+            {
+                info: 'Clean & Commented Code'
+            },
+            {
+                info: 'Highly Customizable'
+            }
     ],
         showDetail: false
     }
